93b6ef46d464c197ed2396730c8f54ebfb1475b0
Avraam Piperidis <piperidis3@yahoo.gr>
2018-02-19 03:36:19 +0200

    Update SkeletonBuilderTest.java

--- src/test/java/com/protectsoft/mediator/SkeletonBuilderTest.java
+++ src/test/java/com/protectsoft/mediator/SkeletonBuilderTest.java
@@ -38,7 +38,7 @@ public class SkeletonBuilderTest {
         String result = instance.build();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
     
 }
